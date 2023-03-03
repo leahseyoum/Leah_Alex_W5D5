0be7a90c1b7219b3b2c0e808878a935db4408227
Leah Seyoum <leahseyoum@gmail.com>
2023-03-03 14:52:42 -0800

    01_queries

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -7,6 +7,7 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
 
+  Movie.where(score: (2..3)).select(:id, :title, :score)
 end
 
 def harrison_ford
@@ -21,6 +22,7 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
 
+  Movie.joins(:actors).where("name = 'Harrison Ford' AND ord > 1").select(:id, :title)
 end
 
 def biggest_cast
@@ -37,7 +39,8 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
-  
+
+  Movie.joins(:castings).group(:id).order('COUNT(castings.id) DESC').limit(3).select(:id, :title)
 end
 
 def directed_by_one_of(them)
@@ -55,6 +58,8 @@ def directed_by_one_of(them)
   
   # Note: Directors appear in the 'actors' table.
 
+  Movie.joins(:director).where(actors: { name: them } ).select(:id, :title)
+
 end
 
 def movie_names_before_1940
@@ -69,4 +74,5 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
+  Movie.where('yr < 1940').pluck(:title)
 end
